511e75abd33a6976a6db12efaad9d1dbf9c69358
Tal Wrii <talwrii@gmail.com>
2025-04-25 13:10:18 +0200

    release 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='kitty-plotnine',
-    version="1.0.0",
+    version="1.1.0",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
